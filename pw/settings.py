98c83607c42a3b7d6b3ee998fb6e470ff3e5e111
Rasul Kireev <kireevr1996@gmail.com>
2019-07-06 23:13:46 +0000

    PRE FINAL changes

--- pw/settings.py
+++ pw/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for pw project.
 
@@ -13,27 +14,20 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from decouple import config, Csv
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
-
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
-
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -84,7 +78,6 @@ WSGI_APPLICATION = 'pw.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -97,7 +90,6 @@ DATABASES = {
 }
 
 # TinyMCE Configuration
-
 TINYMCE_DEFAULT_CONFIG = {
     'theme': "advanced",
 }
@@ -135,7 +127,6 @@ USE_L10N = True
 USE_TZ = True
 
 # S3 Static settings
-# Used this guide
 
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
@@ -147,18 +138,15 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 
 AWS_LOCATION = 'static'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-# DEFAULT_FILE_STORAGE = 'pw.storage_backends.MediaStorage'  # <-- here is where we reference it
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static/'), ]
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+MEDIA_ROOT = [os.path.join(BASE_DIR, 'media/'), ]
+DEFAULT_FILE_STORAGE = 'pw.storage_backends.MediaStorage'  
+MEIDA_URL = 'https://%s/media/' % (AWS_S3_CUSTOM_DOMAIN)
+
+# STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
