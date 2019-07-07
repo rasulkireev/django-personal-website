559b731a18cf6d7eba7dd108945f0683890687b4
Rasul Kireev <kireevr1996@gmail.com>
2019-07-07 13:08:26 -0400

    bb

--- pw/settings.py
+++ pw/settings.py
@@ -144,8 +144,9 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 MEDIA_ROOT = [os.path.join(BASE_DIR, 'media/'), ]
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = 'https://%s/media/' % (AWS_S3_CUSTOM_DOMAIN)
+MEIDA_URL = '/media/'
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#MEDIA_URL = 'https://%s/media/' % (AWS_S3_CUSTOM_DOMAIN)
 
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
