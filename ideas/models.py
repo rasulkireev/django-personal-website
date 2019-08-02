4ecc5fdc1b379eb7e39fcf1970ee9dc68325ed7e
Rasul Kireev <kireevr1996@gmail.com>
2019-08-02 00:34:13 -0400

    redesign

--- ideas/models.py
+++ ideas/models.py
@@ -3,9 +3,19 @@ from autoslug import AutoSlugField
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Idea(models.Model):
-    date = models.DateField()
-    topic = models.CharField(max_length=200)
-    body = models.TextField()
+    title = models.CharField(max_length=200)
+    working = models.BooleanField()
+    description = models.TextField(blank=True)
+    ease = models.IntegerField(validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)])
+    interest = models.IntegerField(validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)])
+    profit = models.IntegerField(validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)])
+
 
     def __str__(self):
         return self.title
--- journal/models.py
+++ journal/models.py
@@ -4,18 +4,9 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Journal(models.Model):
-    title = models.CharField(max_length=200)
-    working = models.BooleanField()
-    description = models.TextField(blank=True)
-    ease = models.IntegerField(validators=[
-            MaxValueValidator(10),
-            MinValueValidator(1)])
-    interest = models.IntegerField(validators=[
-            MaxValueValidator(10),
-            MinValueValidator(1)])
-    profit = models.IntegerField(validators=[
-            MaxValueValidator(10),
-            MinValueValidator(1)])
-
+    date = models.DateField()
+    topic = models.CharField(max_length=200)
+    body = models.TextField()
+        
     def __str__(self):
         return self.title
