0c2c252253d90e8767039df3037209fb35e5aae3
Rasul Kireev <kireevr1996@gmail.com>
2019-09-20 01:57:22 -0400

    V1

--- writings/models.py
+++ writings/models.py
@@ -12,10 +12,23 @@ class Post(models.Model):
     cover = models.ImageField(upload_to='post-images/', blank=True)
     date = models.DateTimeField()
     tags = models.CharField(max_length=200, blank=True)
-    category = models.CharField(max_length=200, blank=True)
+
+    Coding = 'Coding'
+    Cooking = 'Cooking'
+    Personal = "Personal"
+    POST_TYPE = [
+        (Coding, 'Coding'),
+        (Cooking, 'Cooking'),
+        (Personal, 'Personal'),
+    ]
+    category = models.CharField(
+        max_length=20,
+        choices=POST_TYPE,
+        default=Personal,
+    )
 
     def __str__(self):
-        return "(Draft = " + str(self.draft) + ")    " + self.category + ': ' + self.title
+        return "(Draft = " + str(self.draft) + ") " + self.category + ': ' + self.title
 
     def get_absolute_url(self):
         return reverse('writings.views.PostDetailView', args=[str(self.slug)])
