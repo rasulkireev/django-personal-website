0cf409a4585b4f9505bf73a8d94e53fb49d69f09
Rasul Kireev <kireevr1996@gmail.com>
2019-11-22 16:48:55 -0500

    redesign

--- books/models.py
+++ books/models.py
@@ -7,9 +7,8 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     buylink = models.URLField(max_length=200, blank=True)
-    slug = AutoSlugField(populate_from='title', unique_with='title', editable=True)
+    slug = AutoSlugField(populate_from='title', unique_with='title', always_update=True)
     date_read = models.DateField()
-    intro_thoughts = models.TextField(blank=True)
     description = models.TextField(blank=True)
     rank = models.IntegerField(blank=True, validators=[
             MaxValueValidator(10),
