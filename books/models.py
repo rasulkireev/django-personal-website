43649b74639f8c13adbaa38a04bffc196178028d
Rasul Kireev <kireevr1996@gmail.com>
2019-12-13 13:27:06 -0500

    Added Status to Books

--- books/models.py
+++ books/models.py
@@ -3,18 +3,32 @@ from autoslug import AutoSlugField
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Book(models.Model):
-    draft = models.BooleanField(default = False)
+    statuses = [
+        ('READ', 'READ'),
+        ('TO READ', 'TO READ'),
+        ('READING','READING'),
+    ]
+    
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
+    date_read = models.DateField()
+    cover = models.ImageField(upload_to='book-cover/')
+    status = models.CharField(
+        max_length=100,
+        choices=statuses,
+        default='TO READ'
+    )
+    
+    
     main_category = models.CharField(max_length=200, blank=True)
     buylink = models.URLField(max_length=200, blank=True)
-    slug = AutoSlugField(populate_from='title', unique_with='title', always_update=True)
-    date_read = models.DateField()
     description = models.TextField(blank=True)
     rank = models.IntegerField(blank=True, validators=[
             MaxValueValidator(10),
             MinValueValidator(1)])
-    cover = models.ImageField(upload_to='book-cover/')
+    
+    
+    slug = AutoSlugField(populate_from='title', unique_with='title', always_update=True)
 
     def __str__(self):
-        return self.title + " (Draft = " + str(self.draft) + ")"
+        return self.title + " (Status = " + str(self.status) + ")"
--- pages/templatetags/book_tags.py
+++ pages/templatetags/book_tags.py
@@ -6,5 +6,5 @@ from books.models import Book
 
 @register.inclusion_tag('template_tags/book_list.html')
 def latest_books(book):
-  book = Book.objects.filter(draft=False).order_by('-date_read')[0:4]
+  book = Book.objects.filter(status="READ").order_by('-date_read')[0:5]
   return {'book': book}
