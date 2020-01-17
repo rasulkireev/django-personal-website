501bfbf94dc9c18a10ee53f23c2c8138ff8761be
Rasul Kireev <kireevr1996@gmail.com>
2020-01-17 01:59:45 -0500

    Solved two issues surroinding Creation of SQL tables (Mentions) and get absolute url

--- writings/urls.py
+++ writings/urls.py
@@ -3,7 +3,7 @@ from .views import PostListView, PostDetailView, DjangoLatestEntriesFeed, Writin
 
 urlpatterns = [
         path('', PostListView.as_view(), name='all_posts'),
-        path('<slug:slug>', PostDetailView.as_view(),kwargs={'model_name': 'writings.models.MentionableExample'}, name='post'),
+        path('<slug:slug>', PostDetailView.as_view(),kwargs={'model_name': 'writings.Post'}, name='post'),
         path('django-feed/rss/', DjangoLatestEntriesFeed(), name='django-rss'),
         path('feed/rss/', WritingsFeed(), name='writings-rss')
             ]
