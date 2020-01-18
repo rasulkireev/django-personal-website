8dbbec5cffc93c1aa85095c3f0935edea61f40d4
Rasul Kireev <kireevr1996@gmail.com>
2020-01-17 19:42:35 -0500

    Changed urls.py

--- writings/urls.py
+++ writings/urls.py
@@ -3,6 +3,6 @@ from .views import PostListView, PostDetailView, DjangoLatestEntriesFeed
 
 urlpatterns = [
         path('', PostListView.as_view(), name='all_posts'),
-        path('<slug:slug>', PostDetailView.as_view(),kwargs={'app_label': 'writings.Post'}, name='post'),
+        path('<slug:slug>', PostDetailView.as_view(),kwargs={'model_name': 'writings.Post'}, name='post'),
         path('django-feed/rss/', DjangoLatestEntriesFeed(), name='django-rss'),
             ]
