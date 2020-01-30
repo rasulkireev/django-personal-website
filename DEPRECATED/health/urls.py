0b91b00c0ddaa536226c58032f12688ca2207885
Rasul Kireev <kireevr1996@gmail.com>
2020-01-29 20:36:37 -0500

    Added chat via Crisp

--- DEPRECATED/health/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import WeightListView
-
-urlpatterns = [
-        path('', WeightListView.as_view(), name='health-home'),
-]
--- DEPRECATED/wiltw/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import WiltwListView, WiltwDetailView, WiltwLatestView
-
-urlpatterns = [
-        path('archive', WiltwListView.as_view(), name='wiltw_archive'),
-        path('archive/<slug:slug>', WiltwDetailView.as_view(), name='wiltw_then'),
-        path('', WiltwLatestView.as_view(), name='wiltw_now'),
-            ]
